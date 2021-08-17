adb83bc7769df48ffc7744961488765bc8af2a13
Volkov-D-A <dimas.volkov@gmail.com>
2021-08-17 17:20:28 +0500

    config package

--- cmd/vk-bot/main.go
+++ cmd/vk-bot/main.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"projects/vk-stitch-bot/pkg/config"
+)
+
+func main() {
+	if err := run(); err != nil {log.Fatal(err) }
+}
+
+func run() error {
+	cfg := config.GetConfig()
+	fmt.Println(cfg)
+	return nil
+}
--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,1 +1,28 @@
 package config
+
+import (
+	"errors"
+	"os"
+	"sync"
+)
+
+var (
+	EnvParamNotExist = errors.New("environment parameter not exist")
+	EnvParamIsEmpty = errors.New("environment parameter is empty")
+)
+
+type Config struct {
+	Token string
+}
+
+var (
+	once sync.Once
+	config Config
+)
+
+func GetConfig() *Config {
+	once.Do(func() {
+		config.Token = os.Getenv("VK_TOKEN")
+	})
+	return &config
+}
