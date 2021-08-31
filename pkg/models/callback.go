b455798241bdce52c49ad0fcf836c016c663a7fc
Volkov-D-A <dimas.volkov@gmail.com>
2021-08-31 15:20:29 +0500

    :card_file_box: added models package

---
+++ pkg/models/callback.go
@@ -0,0 +1,32 @@
+package models
+
+import "encoding/json"
+
+//CallbackRequest base struct for callback requests from VK api
+type CallbackRequest struct {
+	EventType   string          `json:"type"`
+	EventObject json.RawMessage `json:"object"`
+	GroupId     int             `json:"group_id"`
+	EventId     string          `json:"event_id"`
+	Secret      string          `json:"secret"`
+}
+
+//TypeMessageNew struct for handle "message_new" callback requests from VK api
+type TypeMessageNew struct {
+	Message `json:"message"`
+	Client  `json:"client_info"`
+}
+
+//Message a part for TypeMessageNew struct
+type Message struct {
+	MessageDate   int    `json:"date"`
+	MessageFromId int    `json:"from_id"`
+	MessageId     int    `json:"id"`
+	MessageText   string `json:"text"`
+}
+
+//Client a part for TypeMessageNew struct
+type Client struct {
+	ClientKeyboard       bool `json:"keyboard"`
+	ClientInlineKeyboard bool `json:"inline_keyboard"`
+}
